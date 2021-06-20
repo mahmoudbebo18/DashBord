3348e4190a5186b91fd313002115ff3f623124dd
mahmoudelkhateb <bebobest2014@gmail.com>
2021-06-20 22:28:57 +0200

    users

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$('.adding-new-opt a').click(function() {
+    $('.add-new-opt').slideToggle()
+})
+
+
+$(document).ready(function() {
+    var $textbox = $("#incdec input");
+
+    $("#up").click(function(e) {
+        e.preventDefault();
+        var value = parseInt($textbox.val(), 10);
+
+        $textbox.val(value + 1);
+    });
+
+    $("#down").click(function(e) {
+        e.preventDefault();
+        var value = parseInt($textbox.val(), 10);
+
+        if (value > 0) {
+            $textbox.val(value - 1);
+        }
+    });
+
+})
